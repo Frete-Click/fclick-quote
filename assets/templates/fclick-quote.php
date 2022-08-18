3aa4fa6cd6916dad46d6ebfb0853f3c43224e08b
devmunds <devmunds@gmail.com>
2022-08-18 02:27:16 -0300

    Add label

--- assets/templates/fclick-quote.php
+++ assets/templates/fclick-quote.php
@@ -46,34 +46,39 @@
           </div>
 
           <div class="dms-col-xs-12 dms-col-sm-12 dms-col-md-12 dms-col-lg-12 dms-col-xl-12">
-               <div class="box-product-data text-center">
-                    <h4>Dados dos produtos</h4>
+               <div class="box-product-data">
+                    <h4 class="text-center">Dados dos produtos</h4>
                
                     <div class="dms-col-xs-12 dms-col-sm-6 dms-col-md-2 dms-col-lg-2 dms-col-xl-2">
+                         <span class="text-label">Nº de Volumes</span>
                          <div class="wpfc-box-control">
                               <input type="number" id="product-quantity" name="product-quantity" placeholder="1" required>
                               <div class="text-suffix">QT</div>
                          </div>
                     </div>
                     <div class="dms-col-xs-12 dms-col-sm-6 dms-col-md-2 dms-col-lg-2 dms-col-xl-2">
+                         <span class="text-label">Peso</span>
                          <div class="wpfc-box-control">
                               <input type="text" id="product-weight" name="product-weight" placeholder="0,000 kg" required> 
                               <div class="text-suffix">kg</div>
                          </div>
                     </div>
                     <div class="dms-col-xs-12 dms-col-sm-6 dms-col-md-2 dms-col-lg-2 dms-col-xl-2">
+                         <span class="text-label">Altura</span>
                          <div class="wpfc-box-control">
                               <input type="text" class="product-cm" id="product-height" name="product-height" placeholder="0,00 cm" required>
                               <div class="text-suffix">cm</div>
                          </div>
                     </div>
                     <div class="dms-col-xs-12 dms-col-sm-6 dms-col-md-2 dms-col-lg-2 dms-col-xl-2">
+                         <span class="text-label">Largura</span>
                          <div class="wpfc-box-control">
                               <input type="text" class="product-cm" id="product-width" name="product-width" placeholder="0,00 cm" required>
                               <div class="text-suffix">cm</div>
                          </div>
                     </div>
                     <div class="dms-col-xs-12 dms-col-sm-12 dms-col-md-2 dms-col-lg-2 dms-col-xl-2">
+                         <span class="text-label">Comprimento</span>
                          <div class="wpfc-box-control">
                               <input type="text" class="product-cm" id="product-depth" name="product-depth" placeholder="0,00 cm" required>
                               <div class="text-suffix">cm</div>
